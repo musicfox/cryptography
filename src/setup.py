4e14d50be8b92156c3a69c8db2bbc83ff23e04af
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2021-08-08 12:57:37 -0500

    :recycle: :wrench: add egg_info_ex class

--- src/setup.py
+++ src/setup.py
@@ -8,22 +8,24 @@ import io
 import os
 import sys
 from shutil import rmtree
-
+import mfcrypt
 from setuptools import find_packages, setup, Command
+from setuptools.command.egg_info import egg_info
 
 # Package meta-data.
-NAME = 'mfcrypt'
-DESCRIPTION = 'Cryptographic utilities for Musicfox Python applications.'
-URL = 'https://github.com/cryptography/'
-EMAIL = 'dev@musicfox.io'
-AUTHOR = 'Jason R. Stevens, CFA on behalf of Musicfox, Inc.'
-REQUIRES_PYTHON = '>=3.9'
-VERSION = '0.0.1'
-GEMFURY = f'twine upload dist/* --repository-url https://push.fury.io/musicfox -u {os.environ.get("TWINE_TOKEN")} -p ""' 
+NAME = "mfcrypt"
+DESCRIPTION = "Cryptographic utilities for Musicfox Python applications."
+URL = "https://github.com/cryptography/"
+EMAIL = "dev@musicfox.io"
+LICENSE = "MIT"
+AUTHOR = "Jason R. Stevens, CFA on behalf of Musicfox, Inc."
+REQUIRES_PYTHON = ">=3.9"
+VERSION = mfcrypt.__version__
+GEMFURY = None  # f'twine upload dist/* --repository-url https://push.fury.io/musicfox -u {os.environ.get("TWINE_TOKEN")} -p ""'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'pycryptodomex', 
+    "pycryptodomex",
 ]
 
 # What packages are optional?
@@ -41,8 +43,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
@@ -50,22 +52,22 @@ except FileNotFoundError:
 about = {}
 if not VERSION:
     project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about["__version__"] = VERSION
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -75,57 +77,67 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel distribution…')
-        os.system('{0} setup.py sdist bdist_wheel '.format(sys.executable))
+        self.status("Building Source and Wheel distribution…")
+        os.system("{0} setup.py sdist bdist_wheel ".format(sys.executable))
 
         if GEMFURY:
-            self.status('Uploading the package to Gemfury via Twine...')
+            self.status("Uploading the package to Gemfury via Twine...")
             os.system(GEMFURY)
 
         else:
-            self.status('Uploading the package to PyPI via Twine…')
+            self.status("Uploading the package to PyPI via Twine…")
 
-            os.system('twine upload dist/*')
+            os.system("twine upload dist/*")
 
         sys.exit()
+
+
+class egg_info_ex(egg_info):
+    """Includes license file into `.egg-info` folder."""
+
+    def run(self):
+        # don't duplicate license into `.egg-info` when building a distribution
+        if not self.distribution.have_run.get("install", True):
+            # `install` command is in progress, copy license
+            self.mkpath(self.egg_info)
+            self.copy_file("LICENSE", self.egg_info)
+
+        egg_info.run(self)
 
 
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
     # If your package is a single module, use this instead of 'packages':
-    #py_modules=['mfcrypt'],
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*", "node_modules"]),
+    # py_modules=['mfcrypt'],
+    packages=find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*", "node_modules"]
+    ),
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
     # },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='Proprietary',
+    license=LICENSE,
+    license_files=("LICENSE",),
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: PyPy'
     ],
     # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    cmdclass={"upload": UploadCommand, "egg_info": egg_info_ex},
 )
