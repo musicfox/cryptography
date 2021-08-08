f008830117b0ce7ffc9117558fd99031d23a8a42
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2021-08-08 17:24:00 -0500

    :recycle: :art: move basic styles to subdir

---
+++ examples/javascript/styles/index.js
@@ -0,0 +1,23 @@
+/** Styles for the <p> element */
+export const paragraphStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+  fontSize: "1em",
+  fontWeight: "normal",
+  margin: ".41em 0",
+  textAlign: "left",
+};
+/** Styles for the main container <div> element */
+export const containerStyle = {
+  wordWrap: "break-word",
+  textAlign: "center",
+  margin: "5em 5em 5em 5em",
+  padding: "2em 6em ",
+  backgroundColor: "#e9e9e9",
+  flexWrap: "true",
+};
+/** Styles for the main image <img> element */
+export const imgStyle = {
+  padding: "3rem 0",
+  width: "100%",
+};
